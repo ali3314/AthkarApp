2b29891d2f87569efa3405ada6d2f6873622d3d2
ali3314 <aa4992585@gmail.com>
2022-08-14 09:50:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/athkarapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.athkarapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val secondActivityBtn: Button = findViewById (R.id.secondActivityBtn)
+
+        secondActivityBtn.setOnClickListener() {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
